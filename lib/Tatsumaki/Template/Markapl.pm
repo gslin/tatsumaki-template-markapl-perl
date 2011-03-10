897a04fcca58ff597a3971f852a50ae39fb44448
Gea-Suan Lin <darkkiller@gmail.com>
2011-03-10 17:48:29 +0800

    Import Tatsumaki::Template::Markapl.

---
+++ lib/Tatsumaki/Template/Markapl.pm
@@ -0,0 +1,101 @@
+# ABSTRACT: Use Markapl as Tatsumaki's template system.
+
+package Tatsumaki::Template::Markapl;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Tatsumaki::Template::Markapl - Use Markapl as Tatsumaki's template system.
+
+=head1 DESCRIPTION
+
+This module will use L<Markapl> as Tatsumaki's template system.
+
+=head1 SYNOPSIS
+
+    # This usually is in app.psgi
+    use Tatsumaki::Template::Markapl;
+
+    # Install the template system
+    Tatsumaki::Template::Markapl->rewrite('MyProj::View');
+
+And then in C<MyProj::View>:
+
+    # MyProj::View
+    package MyProj::View;
+    use Markapl;
+
+    template '/' => sub {
+	my $self = shift;
+
+	html {
+	    head {
+		title { 'My Title' };
+	    };
+
+	    body {
+		div('#bd') {
+		    outs('Hello, ');
+		    outs($self->get('name'));
+		};
+	    };
+	};
+    };
+
+Now you can use it in handler:
+
+    #
+    package MyProj::Handler::Index;
+
+    sub get {
+	shift->render('/', {name => 'perl'});
+    };
+
+=cut
+
+use Any::Moose;
+extends 'Tatsumaki::Template';
+
+has 'view_class_name' => (is => 'rw');
+has 'view_class' => (is => 'rw', lazy_build => 1);
+
+use Plack::Util;
+use Sub::Install;
+
+sub _build_view_class {
+    Plack::Util::load_class(shift->view_class_name);
+}
+
+sub render_file {
+    my $self = shift;
+
+    my $str = $self->view_class->render(@_);
+    Plack::Util::inline_object(as_string => sub { $str });
+};
+
+sub rewrite {
+    my ($self, $view_class_name) = @_;
+
+    Sub::Install::reinstall_sub({
+	code => sub { Tatsumaki::Template::Markapl->new(view_class_name => $view_class_name); },
+	into => 'Tatsumaki::Application',
+	as => '_build_template',
+    });
+}
+
+=head1 AUTHOR
+
+Gea-Suan Lin, C<< <gslin at gslin.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Gea-Suan Lin.
+
+This software is released under 3-clause BSD license. See
+L<http://www.opensource.org/licenses/bsd-license.php> for more
+information.
+
+=cut
+
+1;
